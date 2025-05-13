1c400e39ab9d547f214ecd39fa6553f88e5211c6
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2025-05-13 23:52:43 +0200

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,28 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-export default defineConfig({
-  plugins: [vue()],
-  base: '/jedison-docs/',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '/jedison-docs/' : '/'
+
+  return {
+    plugins: [vue()],
+    base,
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src')
+      }
+    },
+    build: {
+      outDir: resolve(__dirname, 'docs'),
+    },
+    server: {
+      port: 8080,
+      strictPort: true
+    },
+    preview: {
+      port: 8081,
+      strictPort: true
     }
-  },
-  build: {
-    outDir: resolve(__dirname, 'docs')
-  },
-  server: {
-    port: 8080
-  },
-  preview: {
-    port: 8081,
-    strictPort: true
   }
 })
