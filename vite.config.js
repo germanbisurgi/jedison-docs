025c8a6a8c094c1cc77ddaa7c402b2dad996e0f9
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2025-05-13 23:28:05 +0200

    Updated vite base to repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/jedison-docs/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
