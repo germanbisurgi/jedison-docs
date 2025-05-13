779ccabb1173f77883ad863cd980e749695e497a
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2025-05-13 23:01:57 +0200

    Updated vite config

--- config/vite.config.js
+++ config/vite.config.js
@@ -4,7 +4,6 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  // assetsInclude: ['**/*.ttf'],
   resolve: {
     alias: {
       '@': resolve(__dirname, '../src')
