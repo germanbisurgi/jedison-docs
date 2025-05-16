0111e1dac1a879d31e3bf69a2b413cd359dacffa
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2025-05-16 09:44:37 +0200

    Deleted content

--- src/pages/Install.vue
+++ src/pages/Install.vue
@@ -26,26 +26,6 @@
       <article>
         <app-highlight language="html" :code="mdUsingCdn"/>
       </article>
-
-      <article>
-        <p>To use Jedison as a JSON editor, this options are required:</p>
-
-        <ul>
-          <li>
-            <strong><code>container</code></strong> specifies where the editor renders in your HTML. Without it, Jedison has no place to display the form. In the example, we attach
-            it
-            to <code>&lt;div id="jedison-container"&gt;</code> using <code>document.querySelector</code>.
-          </li>
-          <li>
-            <strong><code>theme</code></strong> defines the editor's visual style. While Jedison provides a default theme (<code>new Jedison.Theme()</code>), you must include this
-            option—omitting it may break styling or functionality.
-          </li>
-          <li>
-            <strong><code>schema</code></strong> defines the validation rules. The editor will check JSON data
-            against this schema.
-          </li>
-        </ul>
-      </article>
     </section>
   </div>
 </template>
