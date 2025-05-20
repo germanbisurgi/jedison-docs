a5a41ccd40d6a1995d707cc0004b1dadc31ca92f
German Bisurgi <g.bisurgi@herzogkommunikation.de>
2025-05-20 10:06:03 +0200

    Codemirror fixes

--- src/components/AppCodemirror.vue
+++ src/components/AppCodemirror.vue
@@ -3,13 +3,13 @@
 </template>
 
 <script>
-import {EditorView, keymap} from '@codemirror/view'
-import {EditorState} from '@codemirror/state'
-import {defaultKeymap} from '@codemirror/commands'
-import {html} from '@codemirror/lang-html'
-import {oneDark} from '@codemirror/theme-one-dark'
-import {githubLight, githubDark} from '@uiw/codemirror-theme-github'
-import {dracula} from '@uiw/codemirror-theme-dracula'
+import { EditorView, keymap } from '@codemirror/view'
+import { EditorState } from '@codemirror/state'
+import { defaultKeymap } from '@codemirror/commands'
+import { html } from '@codemirror/lang-html'
+import { oneDark } from '@codemirror/theme-one-dark'
+import { githubLight, githubDark } from '@uiw/codemirror-theme-github'
+import { dracula } from '@uiw/codemirror-theme-dracula'
 
 export default {
   name: 'HtmlCodeMirrorEditor',
@@ -44,10 +44,10 @@ export default {
   computed: {
     currentTheme() {
       const themes = {
-        oneDark: oneDark,
-        githubLight: githubLight,
-        githubDark: githubDark,
-        dracula: dracula,
+        oneDark,
+        githubLight,
+        githubDark,
+        dracula,
         default: null
       }
       return themes[this.theme]
@@ -88,8 +88,8 @@ export default {
     },
     initEditor() {
       const extensions = [
-        keymap.of(defaultKeymap), // This fixes the Enter key issue
         html(),
+        keymap.of(defaultKeymap || []), // ✅ Safe fallback for build
         EditorView.updateListener.of(update => {
           if (update.docChanged) {
             const content = update.state.doc.toString()
@@ -113,6 +113,8 @@ export default {
         state: editorState,
         parent: this.$refs.editorContainer
       })
+
+      this.editorView.focus() // ✅ Fix for Enter key issue in Chrome
 
       this.editorValue = editorState.doc.toString()
     },
